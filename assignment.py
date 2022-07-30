d8109b3c36bbb78b23994e2cfc98264dc51fc385
Rose Tovar <roseatovar@gmail.com>
2022-07-30 16:01:39 -0500

    notes

--- assignment.py
+++ assignment.py
@@ -1,8 +1,86 @@
-####
-# 1. Create an If Else Statement that checks peoples ages and gives them access based on age
+from functools import cache
+from gettext import find
 
 
-# 2. Create a while loop that counts to 50
+def printer(name, age):
+    print(name)
+    print(age)
 
 
-# 3. Create a for loop that prints numbers 1 to 20
+# printer('rose', 32)
+
+
+def anyLength(*args):
+    print(args)
+    for item in args:
+        print(item)
+
+
+# anyLength(10)
+# anyLength(10, 20)
+
+
+def calculation(a, b):
+    sum = a + b
+    sub = a - b
+
+    return sum, sub
+
+
+# (sum, sub) = calculation(10, 20)
+# print(sum, sub)
+
+def printEmployee(name, salary=9000):
+    print(name)
+    print(salary)
+
+
+# printEmployee("Rose", 65000)
+
+# printEmployee('Nick')
+
+
+def adder(a, b):
+    def insideAdder(a, b):
+        return a + b
+
+    sum = insideAdder(a, b)
+    return sum + 5
+
+
+# print(adder(10, 20))
+
+def recurAdder(num):
+    if num == 0:
+        return 0
+    else:
+        return num + recurAdder(num-1)
+
+
+print(recurAdder(10))
+
+
+def display_student(name, age):
+    print(name, age)
+
+
+display_student("Emma", 26)
+
+showStudent = display_student
+
+showStudent('Rose', 31)
+
+print(list(range(4, 30, 4)))
+
+
+def findlargest(nums):
+    largest = -9999999999
+    for num in nums:
+        if num > largest:
+            largest = num
+
+    return largest
+
+
+x = [4, 6, 8, 24, 12, 2]
+print(findlargest(x))
