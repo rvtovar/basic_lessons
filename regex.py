aa3ae6a22ee244fa7c86905fc32796b2e6f17af5
Rose Tovar <roseatovar@gmail.com>
2022-08-17 15:57:30 -0500

    update

--- regex.py
+++ regex.py
@@ -1,4 +1,5 @@
 # # Regular Expressions
+from atexit import register
 import re
 
 
@@ -134,16 +135,111 @@ import re
 
 # Findall method
 
-phoneRegex = re.compile(r"(\d\d\d)-(\d\d\d)-(\d\d\d\d)")
+# phoneRegex = re.compile(r"(\d\d\d)-(\d\d\d)-(\d\d\d\d)")
 
-mo = phoneRegex.search('My numbers are 901-901-0000 and 800-777-8989')
-
-
-print(mo.group())
+# mo = phoneRegex.search('My numbers are 901-901-0000 and 800-777-8989')
 
 
-numbers = phoneRegex.findall('My numbers are 901-901-0000 and 800-777-8989')
+# print(mo.group())
 
-print(numbers[0])
 
-tuples = ('hello', 'Hello')
+# numbers = phoneRegex.findall('My numbers are 901-901-0000 and 800-777-8989')
+
+# print(numbers[0])
+
+
+"""
+\d any numeric digit
+\D any non numeric character
+\w Any letter, numeric character, or underscore
+\W any character not a letter, digit, or underscore
+\s any space tab, or new line
+\S Any character not a space tab or new line
+
+"""
+
+# xmasRegex = re.compile(r"\d+\s\w+")
+
+# print(xmasRegex.findall('12 drummers, 11 pipers, 1 partridge'))
+
+
+# vowelRegex = re.compile(r"[^aeiouAEIOU]")
+
+# print(vowelRegex.findall('Robocop eats babyfood. BABY FOOD'))
+
+
+# re.compile(r"[^a-zA-Z0-9]")
+
+# Carrot Symbol and Dollar Sign
+
+# ro = re.compile(r"^Hello")
+
+# print(ro.search("Hello World").group())
+
+
+# ends = re.compile(r"\d$")
+
+# print(ends.search('42').group())
+
+
+# whole = re.compile(r"^\d+$")
+
+# print(whole.search('90d2'))
+
+
+# The Wild Card Method
+
+# atRegex = re.compile(r".at")
+
+# print(atRegex.findall('Cat Bat bat cat mat sat hat'))
+
+
+# nameRegex = re.compile(r"First Name: (.*) Last Name: (.*)")
+
+# mo = nameRegex.search("First Name: John Last Name: Smith")
+
+# print(mo.groups())
+
+
+# non = re.compile(r"<.*?>")
+
+# mo = non.search('<Help im> But im >')
+
+# print(mo.group())
+
+# greedy = re.compile(r"<.*>")
+
+# mo = greedy.search('<Help im> But im >')
+
+# print(mo.group())
+
+
+# newline = re.compile('.*', re.DOTALL)
+
+# print(newline.search('Serve the public\nProtect the innocent'))
+
+
+# Case sensitivity
+
+robocop = re.compile(r'robocop', re.I)
+
+print(robocop.search('RoboCop'))
+
+nameRegex = re.compile(r"Agent (\w)\w*")
+
+print(nameRegex.sub('CENSORED',
+      'Agent Alice told Agent Carol that Agent Eve knows Agent Bob was a double agent'))
+
+
+print(nameRegex.sub(r'\1****',
+      'Agent Alice told Agent Carol that Agent Eve knows Agent Bob was a double agent'))
+
+
+phoneRegex = re.compile(r'''(
+    \d{3}|\(\d{3}\))?
+    (\s|-|\.)
+    \d{3}
+    (\s|-|\.)
+    \d{4}
+    (\s*(ext|x|ext.)\s*\d{2,5})?
+''', re.VERBOSE)
